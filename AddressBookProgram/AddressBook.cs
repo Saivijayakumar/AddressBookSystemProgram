69cf8ed6b4ac38f610ec61db4439f43d161c60d0
saivijayakumar <bollusaivijaykumar212@gmail.com>
2021-07-03 22:43:20 +0530

    [SaiVijayaKumar] Adding New Contact to Address Book

---
+++ AddressBookProgram/AddressBook.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookProgram
+{
+    class AddressBook
+    {
+        List<StoreDetails> storeDetails;
+
+        public AddressBook()
+        {
+            storeDetails = new List<StoreDetails>();
+        }
+
+        public bool AddNewContact(string firstName, string lastName, string address, string city, string state, int zip, long phoneNumber, string email)
+        {
+            StoreDetails store = new StoreDetails(firstName,lastName, address,  city,  state,  zip,  phoneNumber,  email);
+            StoreDetails result = CheckingExistence(firstName);
+            if (result == null)
+            {
+                storeDetails.Add(store);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public StoreDetails CheckingExistence(string firstName)
+        {
+            StoreDetails store = storeDetails.Find((a) => a.firstName == firstName);
+            return store;
+        }
+    }
+}
--- AddressBookProgram/Program.cs
+++ AddressBookProgram/Program.cs
@@ -11,12 +11,72 @@ namespace AddressBookProgram
     /// </summary>
     class Program
     {
+        public AddressBook book = new AddressBook();
+        public string firstName;
+        public string lastName;
+        public string address;
+        public string city;
+        public string state;
+        public int zip;
+        public long phoneNumber;
+        public string email;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Adress Book Program");
-            //ReadInput();
-            StoreDetails storeDetails = new StoreDetails("sai", "vijaya", "address", "city", "state", 123, 123456789, "something");
-            Console.Read();
+            Console.WriteLine("\t===> LIST of Features <====");
+            Console.WriteLine("\t----------------------------------------------");
+            Console.WriteLine("\t 1.Adding New Contact\n\t 0.For Exit");
+            Console.WriteLine("\t----------------------------------------------");
+        
+            Program program = new Program();
+            program.FeaturesList();
+            
+        }
+        public void FeaturesList()
+        {
+            bool simply = true;
+            while (simply)
+            {
+                Console.Write("\n\tEnter your choice : ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("\n\t Adding New Contact Please Enter Details");
+                        Console.Write("Enter the First Name : ");
+                        firstName = Console.ReadLine();
+                        Console.Write("Enter the Last Name : ");
+                        lastName = Console.ReadLine();
+                        Console.Write("Enter the Address : ");
+                        address = Console.ReadLine();
+                        Console.Write("Enter the City Name : ");
+                        city = Console.ReadLine();
+                        Console.Write("Enter the State Name : ");
+                        state = Console.ReadLine();
+                        Console.Write("Enter the zip code : ");
+                        zip = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Enter the Phone Number : ");
+                        phoneNumber = long.Parse(Console.ReadLine());
+                        Console.Write("Enter the email address : ");
+                        email = Console.ReadLine();
+                        if (book.AddNewContact(firstName, lastName, address, city, state, zip, phoneNumber, email))
+                        {
+                            Console.WriteLine("Contact Is Added Successfully");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Existing Contact");
+                        }
+                        break;
+                    case 0:
+                        simply = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("\t PLEASE ENTER A VALID OPTION");
+                        break;
+                }
+            }
         }
         
     }
